6c445bee8c7a11e5eb3afc9f5e5cc70807f49013
Joey-Herrera <joseph.herrera@utexas.edu>
2021-04-14 17:29:24 -0500

    Create Append_CPS_data.R

---
+++ RDD2/R Scripts/Append_CPS_data.R
@@ -0,0 +1,27 @@
+# Load in required packages
+library(haven)
+library(tidyverse)
+
+# Create a path to read data from the source
+read_data <- function(df)
+{
+  full_path <- paste("https://raw.github.com/scunning1975/mixtape/master/", 
+                     df, sep = "")
+  df <- read_dta(full_path)
+  return(df)
+}
+
+nsw_dw <- read_data("nsw_mixtape.dta")
+
+# Append variables as necessary
+nsw_dw_cpscontrol <- read_data("cps_mixtape.dta") %>% 
+  bind_rows(nsw_dw) %>% 
+  mutate(agesq = age^2,
+         agecube = age^3,
+         educsq = educ*educ,
+         u74 = case_when(re74 == 0 ~ 1, TRUE ~ 0),
+         u75 = case_when(re75 == 0 ~ 1, TRUE ~ 0),
+         interaction1 = educ*re74,
+         re74sq = re74^2,
+         re75sq = re75^2,
+         interaction2 = u74*hisp)
