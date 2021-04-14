a7b284a9967e8018aaafad8e679ad3f6ec34ca58
Joey-Herrera <joseph.herrera@utexas.edu>
2021-04-14 17:29:48 -0500

    Update nsw_pscore_data.R

--- RDD2/Data/nsw_pscore_data.R
+++ RDD2/Data/nsw_pscore_data.R
@@ -2,6 +2,5 @@
 # Install the haven library to gain access to the read_dta function
 library(haven)
 # Load data
-nsw_pscore = read_dta("https://github.com/scunning1975/mixtape/raw/master/nsw_mixtape.dta")
+nsw_dw = read_dta("https://github.com/scunning1975/mixtape/raw/master/nsw_mixtape.dta")
 
-h
