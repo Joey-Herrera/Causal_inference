adda683348b2bd481f4bd89212d654b30b1cecd7
Joey-Herrera <joseph.herrera@utexas.edu>
2021-04-15 12:01:16 -0500

    Update Append_CPS_data.R

--- RDD2/R Scripts/Append_CPS_data.R
+++ RDD2/R Scripts/Append_CPS_data.R
@@ -14,14 +14,33 @@ read_data <- function(df)
 nsw_dw <- read_data("nsw_mixtape.dta")
 
 # Append variables as necessary
-nsw_dw_cpscontrol <- read_data("cps_mixtape.dta") %>% 
+nsw_lm <- read_data("cps_mixtape.dta") %>% 
   bind_rows(nsw_dw) %>% 
   mutate(agesq = age^2,
          agecube = age^3,
          educsq = educ*educ,
+         educcube = educ^3,
          u74 = case_when(re74 == 0 ~ 1, TRUE ~ 0),
          u75 = case_when(re75 == 0 ~ 1, TRUE ~ 0),
-         interaction1 = educ*re74,
-         re74sq = re74^2,
+         re74sq = re74^2, 
+         re74cube = re74^3,
          re75sq = re75^2,
-         interaction2 = u74*hisp)
+         re75cube = re75^3,
+         re78sq = re78^2,
+         re75cube = re78^3,
+         )
+
+nsw_logit <- read_data("cps_mixtape.dta") %>% 
+  bind_rows(nsw_dw) %>% 
+  mutate(agesq = age^2,
+         agecube = age^3,
+         educsq = educ*educ,
+         educcube = educ^3,
+         u74 = case_when(re74 == 0 ~ 1, TRUE ~ 0),
+         u75 = case_when(re75 == 0 ~ 1, TRUE ~ 0),
+         re74sq = re74^2, 
+         re74cube = re74^3,
+         re75sq = re75^2,
+         re75cube = re75^3,
+         re78sq = re78^2,
+         re75cube = re78^3)
